5303d14b8c310308dfaf75db1f517aadf54c9f32
Stefano <ssteee@gmail.com>
2016-12-07 18:14:06 +0200

    Added private variables

---
+++ app/private_variables.py
@@ -0,0 +1,2 @@
+client_id = 'f272f4a3-ecc1-44fe-b3f4-9a20e9433f4e'
+client_secret = '534cda42-719c-4b26-86c2-c96b7cb03437'
--- app/server.py
+++ app/server.py
@@ -9,9 +9,7 @@ from static_files import app as static_files_app
 demo_app.merge(static_files_app)
 demo_app.merge(generate_button_app)
 
-print(os.environ.get('APP_LOCATION'))
-
 if os.environ.get('APP_LOCATION') == 'heroku':
-  run(demo_app, host='0.0.0.0', port=int(os.environ.get('PORT'), 5000))
+  run(demo_app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 else:
   run(demo_app, host='127.0.0.1', port=8000, reloader=True)
