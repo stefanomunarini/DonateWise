3385b3494d2da0995c346249e6e40cf72f398faf
Stefano <ssteee@gmail.com>
2016-12-07 17:59:42 +0200

    Add Procfile

--- app/server.py
+++ app/server.py
@@ -1,9 +1,17 @@
+import os
+
 from bottle import run
 
 from demo import app as demo_app
-from generate_button import app as generate_button_app
+from button_generator import app as generate_button_app
 from static_files import app as static_files_app
 
 demo_app.merge(static_files_app)
 demo_app.merge(generate_button_app)
-run(demo_app, host='127.0.0.1', port=8000, reloader=True)
+
+print(os.environ.get('APP_LOCATION'))
+
+if os.environ.get('APP_LOCATION') == 'heroku':
+  run(demo_app, host='0.0.0.0', port=int(os.environ.get('PORT'), 5000))
+else:
+  run(demo_app, host='127.0.0.1', port=8000, reloader=True)
